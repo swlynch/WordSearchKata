4f3d4dc3c736d76d471a1a0b0b072609fd002d7c
Steve Lynch <stephenwlynch@gmail.com>
2017-11-11 16:08:08 -0500

    Wrote second failed test to test finding index of new word

--- src/test/java/com/techelevator/WordSearchTest.java
+++ src/test/java/com/techelevator/WordSearchTest.java
@@ -5,8 +5,14 @@ import org.junit.*;
 public class WordSearchTest {
 
 	@Test
-	public void returnsIndexOfFirstLetterInSingleLine() {
+	public void shouldReturnAValueOf0ForIndexOfWord() {
 		WordGrid grid = new WordGrid(); 
 		Assert.assertEquals("Should return the index 0", 0, grid.find("WORD"));
+	}
+	
+	@Test
+	public void shouldReturnAValueOf1ForIndexOfOr() {
+		WordGrid grid = new WordGrid();
+		Assert.assertEquals("Should return the index 1", 1, grid.find("OR"));
 	}
 }
