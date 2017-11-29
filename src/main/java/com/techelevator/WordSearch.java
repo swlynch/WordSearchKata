fea7cb75f7a61f23ddbbe9a08b3f1419bda804e3
Steve Lynch <stephenwlynch@gmail.com>
2017-11-28 20:07:19 -0500

    Created WordSearch class that holds a grid and a string array of words to search for

---
+++ src/main/java/com/techelevator/WordSearch.java
@@ -0,0 +1,22 @@
+package com.techelevator;
+
+public class WordSearch {
+
+	private Grid grid;
+	private String[] searchWords;
+	
+	public Grid getGrid() {
+		return grid;
+	}
+	public void setGrid(Grid grid) {
+		this.grid = grid;
+	}
+	public String[] getSearchWords() {
+		return searchWords;
+	}
+	public void setSearchWords(String[] searchWords) {
+		this.searchWords = searchWords;
+	}
+	
+	
+}
